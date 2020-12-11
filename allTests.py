0df0dd438cc551b623d0d919d584e102ca1871bf
arafMustavi <arafmustavi@gmail.com>
2020-12-12 02:15:47 +0600

    :hand: Serial Test Ongoing

--- allTests.py
+++ allTests.py
@@ -1,20 +1,16 @@
-# IMPORT MODULES
+# IMPORT NECESSARY MODULES
 import numpy as np
+import math
 from ZValues import randNumGenerator
+from scipy import stats
 
 # PARAMETERS
 n = 40
 SEED = 1505009
 k = 9
 ZU = randNumGenerator(SEED, n)
-###################################################
-# UNIFORMITY TEST
-# TASKS:
-# 01 DIVIDE into k sub-Intervals [DONE]
-# 02 Frequency of Ui in that sub interval [DONE]
-# 03 Calculate Kai Square [DONE]
-###################################################
 Us = ZU[1]
+# ALL THE GENERATED VALUES
 print("ALL U's:")
 print(Us)
 print("maxVal:")
@@ -22,52 +18,62 @@ print(max(Us))
 print("minVal:")
 print(min(Us))
 
+###################################################
+# TEST 01 UNIFORMITY TEST
+# TASKS:
+# 01 DIVIDE into k sub-Intervals [DONE]
+# 02 Frequency of Ui in that sub interval [DONE]
+# 03 Calculate Chi Square [DONE]
+###################################################
+print("\nCURRENT TEST: UNIFORMITY TEST\n")
 # FIND ALL THE SUB INTERVALS
-
 intervals = np.linspace(0, 1, k, endpoint=False)
 intervals = np.append(intervals, [1])
 
-frequencyCount ={}
+frequencyCount = {}
 for i in intervals:
     # print(i)
     frequencyCount[i] = 0
 
 for u in Us:
-    print("WORKING U",u)
-    for i in range(k+1):
+    # print("WORKING U", u)
+    for i in range(k + 1):
         if u <= intervals[i]:
-            frequencyCount[intervals[i-1]] += 1
-            print(u,"Done")
+            frequencyCount[intervals[i - 1]] += 1
+            # print(u, "Done")
             break
-
-print(frequencyCount)
-
-
-kai_square = 0
-kai_sum = 0
-
+# print(frequencyCount)
+chi_square = 0
+chi_sum = 0
 for x in frequencyCount.keys():
-    kai_sum += (frequencyCount[x] - n/k) ** 2
+    chi_sum += (frequencyCount[x] - n / k) ** 2
 
-kai_square = kai_sum*k/n
+chi_square = chi_sum * k / n
 
-print(kai_square)
+# print(chi_square)
 
+###############################
 # NEED TO WORK ON THE STUB CODE
+###############################
+# PARAMETER ALPHA
+alpha = 90
+chi_square_theory = stats.chi2.ppf(q=k - 1, df=1 - alpha)
 
+if chi_square > chi_square_theory:
+    print("UNIFORMITY HYPOTHESIS REJECTED")
+else:
+    print("UNIFORMITY HYPOTHESIS ACCEPTED")
 
 # summer = 0
 # for x in frequencyCount.keys():
 #     summer += frequencyCount[x]
 #
 # print(summer)
-
-def find_range(number,intervals):
-    for i in intervals:
-        if number < i:
-            return i
-
-
+# def find_range(number,intervals):
+#     for i in intervals:
+#         if number < i:
+#             return i
+# CODE FROM PREVIOUS ASSIGNMENT
 #########################################################
 # def generate_experiment_probability(x_theory, y_theory, N):
 # observation = {}
@@ -85,48 +91,42 @@ def find_range(number,intervals):
 # for keys in observation.items():
 #     y_exp.append(keys[1] / N)
 # # return y_exp
-#
-#
-
 
 ###################################################
-# SERIAL TEST
+# UNIFORMITY DONE
+###################################################
+# TEST 02 SERIAL TEST
 # TASKS:
 # 01 DIVIDE into l = [n/d] Tuples
 # 02 Frequency of Ui in that sub interval
 # 03 Calculate Kai Square
 ###################################################
-
-# Us = ZU[1]
-# print("ALL U's:")
-# print(Us)
-# print("maxVal:")
-# print(max(Us))
-# print("minVal:")
-# print(min(Us))
+print("\nCURRENT TEST: SERIAL TEST\n")
 
 # FIND ALL THE TUPLES
+tupleList = []
 # PARAMETERS
 d = 3
-tupleList = []
 
 for i in range(0, len(Us), d):
     temp = tuple(Us[i:i + d])
     tupleList.append(temp)
 
-print(tupleList)
-print(len(tupleList))
+# print(tupleList)
+# print(len(tupleList))
+#####################
+# TRYING THE SERIALS
+#####################
 
 ###################################################
-# RUNS TEST
+# TEST 03 RUNS TEST
 # TASKS:
 # 01 FIND RUN LENGTH
 # 02 HARD CODE Aij Bij
 # 03 CALCULATE CCapitalR
 # 04 Compare with KAI-SQUARE
 ###################################################
-
-# = [[4529.4, 9044.9, 13568, 18091, 22615, 27892],[]]
+print("\nCURRENT TEST: RUNS TEST\n")
 
 aMatrix = [
     [4529.4, 9044.9, 13568, 18091, 22615, 27892],
@@ -147,7 +147,7 @@ runLength = {}
 # for x in range(1, len(testArray)):
 #     runLength[x] = 0
 
-print(runLength)
+# print(runLength)
 # testArray = [86, 11, 23, 3, 13, 6, 55, 64, 87, 110,121,129,130]
 prevIdx = 0
 tempLen = 1
@@ -167,7 +167,6 @@ def groupSequence(lst):
     return res
 
 
-# Driver program
 groups = groupSequence(Us)
 
 for x in groups:
@@ -181,7 +180,7 @@ for x in groups:
         runLength[tempLen] = 0
     runLength[tempLen] += 1
 
-print(runLength)
+# print(runLength)
 
 r = []
 for i in range(1, 6 + 1):
@@ -190,9 +189,9 @@ for i in range(1, 6 + 1):
     else:
         r.append(runLength[i])
 
-print(len(aMatrix))
-print(len(bMatrix))
-print(len(r))
+# print(len(aMatrix))
+# print(len(bMatrix))
+# print(len(r))
 
 # CALCULATE THE R
 capitalR = 0
@@ -203,10 +202,61 @@ for i in range(0, 6):
 
 # DIVIDE BY n
 capitalR /= n
-print(capitalR)
+# print(capitalR)
 
-# #######################
-# COMPARE WITH KAI-SQUARE
-# #######################
 
+# ###############################
+# ONGOING COMPARE WITH CHI-SQUARE
+# ###############################
+# PARAMETER
+alpha = .90
+
+chi_square_theory = stats.chi2.ppf(q=6, df=1 - alpha)
+
+if capitalR >= chi_square_theory:
+    print("RUNS HYPOTHESIS REJECTED")
+else:
+    print("RUNS HYPOTHESIS ACCEPTED")
+##################################################
+# RUNS DONE
+###################################################
+# TEST 04 CORRELATION TEST
+# TASKS:
+# 01 CALCULATE Aj
+# 02 TEST THE NULL HYPOTHESIS
+###################################################
+print("\nCURRENT TEST: CORRELATION TEST\n")
+
+# ro = {}
+# var = {}
+A = {}
+corparam_j = 3
+# for j in range(1000000):
+h = math.floor((n - 1) / corparam_j - 1)
+sumSum = 0
+for k in range(0, h + 1):
+    idx1 = 0 + k * corparam_j
+    idx2 = 0 + (k + 1) * corparam_j
+    product = Us[idx1] * Us[idx2]
+    sumSum += product
+# ro[j] = (12 / (h + 1)) * sumSum - 3
+# var[ro[j]] = (13*h + 7) / ((h+1)**2)
+# A[j] = ro[j] / math.sqrt(var[ro[j]])
+
+ro = (12 / (h + 1)) * sumSum - 3
+var = (13 * h + 7) / ((h + 1) ** 2)
+A[corparam_j] = ro / math.sqrt(var)
+
+# PARAMETER
+corparam_alpha = 0.95
+z = stats.norm.ppf(1 - corparam_alpha / 2)
+
+if abs(A[corparam_j]) > z:
+    print("CORRELATION HYPOTHESIS REJECTED")
+else:
+    print("CORRELATION HYPOTHESIS ACCEPTED")
+
+################################################
+# CORRELATION DONE
+###############################################
 
