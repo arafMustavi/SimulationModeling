fec4e5cf5bd2c76bdc3ac2f242e5abfbb93a9454
arafMustavi <arafmustavi@gmail.com>
2020-12-14 21:22:40 +0600

    :+1: SERIAL Test Added and All Complete

--- allTests.py
+++ allTests.py
@@ -6,8 +6,8 @@ from scipy import stats
 
 # PARAMETERS
 n = 40
-SEED = 1505009
 k = 9
+SEED = 1505009
 ZU = randNumGenerator(SEED, n)
 Us = ZU[1]
 # ALL THE GENERATED VALUES
@@ -117,6 +117,51 @@ for i in range(0, len(Us), d):
 #####################
 # TRYING THE SERIALS
 #####################
+n = 20
+k = 8
+sum = 0
+d = 3
+alpha = 0.1
+arr = []
+l = math.floor(n / d)
+
+dict = {}
+
+for i in range(n // d * d) :
+    arr.append(u[i])
+
+    if len(arr) == d:
+        pattern = []
+        for j in range(d) :
+            idx = int(arr[j] * k)
+            pattern.append(idx)
+
+        pattern = tuple(pattern)
+        if pattern not in dict:
+            dict[pattern] = 0
+
+        dict[pattern] += 1
+        arr = []
+
+zeros = int(math.pow(k, d) - len(dict))
+val = - (l / math.pow(k, d))
+val = math.pow(val, 2)
+chi_square = val * zeros
+
+for key in dict:
+    temp = dict[key] - (l / math.pow(k, d))
+    chi_square += math.pow(temp, 2)
+
+chi_square *= (pow(k, d) / l)
+y = stats.chi2.ppf(q = 1 - alpha, df = pow(k, d) - 1)
+
+print("Chi_square :", round(chi_square, 3))
+print("Analytical Value :", round(y, 3))
+
+if chi_square > y :
+    print("HYPOTHESIS REJECTED")
+else:
+    print("HYPOTHESIS ACCEPTED")
 
 ###################################################
 # TEST 03 RUNS TEST
